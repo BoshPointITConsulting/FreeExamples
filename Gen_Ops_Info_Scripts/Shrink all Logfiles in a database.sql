40fdb8b21287126102e389ae6d52b5d3a495af9a
BoshPointITConsulting <110499501+BoshPointITConsulting@users.noreply.github.com>
2024-01-17 11:12:27 -0500

    Update Shrink all Logfiles in a database.sql

--- Gen_Ops_Info_Scripts/Shrink all Logfiles in a database.sql
+++ Gen_Ops_Info_Scripts/Shrink all Logfiles in a database.sql
@@ -1,11 +1,31 @@
+IF OBJECT_ID(N'tempdb..#tempDatabases2Use') IS NOT NULL
+    BEGIN
+        DROP TABLE #tempDatabases2Use
+    END
+CREATE TABLE #tempDatabases2Use
+([Name] NVARCHAR(256), [database_id] INT, [state_desc] NVARCHAR(256), [is_read_only] bit, [Role_Desc] NVARCHAR(100), [mirroring_role] NVARCHAR(100), [recovery_model_desc] NVARCHAR(25));
+INSERT INTO #tempDatabases2Use
+SELECT DISTINCT sdb.name, sdb.database_id, sdb.state_desc, sdb.is_read_only, hars.role_desc, mr.mirroring_role, recovery_model_desc
+FROM sys.databases sdb
+JOIN SYS.database_mirroring mr on sdb.database_id = mr.database_id
+LEFT JOIN sys.dm_hadr_availability_replica_states hars on sdb.replica_id = hars.replica_id
+LEFT JOIN sys.dm_hadr_database_replica_states sdhdrs ON sdb.database_id = sdhdrs.database_id
+WHERE sdb.name NOT IN ('master','model','msdb','tempdb','distribution');
+DELETE FROM #tempDatabases2Use WHERE Role_Desc = 'SECONDARY';
+DELETE FROM #tempDatabases2Use WHERE Role_Desc = 'RESOLVING';
+DELETE FROM #tempDatabases2Use WHERE state_desc = 'OFFLINE';
+DELETE FROM #tempDatabases2Use WHERE state_desc = 'RESTORING';
+DELETE FROM #tempDatabases2Use WHERE is_read_only = 1;
+SELECT * FROM #tempDatabases2Use
+-----------------------------------
 DECLARE @ScriptToShrinkLogs VARCHAR(MAX);
 SET @ScriptToShrinkLogs = '';
 SELECT
 @ScriptToShrinkLogs = @ScriptToShrinkLogs +
-'USE ['+ d.name +']; CHECKPOINT; DBCC SHRINKFILE ('+f.name+');'
+'USE ['+ temp.name +']; CHECKPOINT; DBCC SHRINKFILE (['+f.name+'],0);'
 FROM sys.master_files f
-INNER JOIN sys.databases d ON d.database_id = f.database_id
-WHERE f.type = 1 AND d.database_id > 4 and f.type_desc = 'log'
+INNER JOIN #tempDatabases2Use temp ON temp.database_id = f.database_id
+WHERE f.type = 1 AND temp.database_id > 4 and f.type_desc = 'log'
 -- AND d.name = 'NameofDB'
 SELECT @ScriptToShrinkLogs ScriptToShrinkLogs
 EXEC (@ScriptToShrinkLogs)
